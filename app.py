ea02e3492c1871927d31e14e7182cad76c7f79a7
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2023-06-12 10:28:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -50,6 +50,18 @@ if Parametros_uploaded_file:
     st.plotly_chart(fig)
 
     # -- DOWNLOAD SECTION
+    
+    def generate_excel_download_link(df):
+        # Credit Excel: https://discuss.streamlit.io/t/how-to-add-a-download-excel-csv-function-to-a-button/4474/5
+        towrite = BytesIO()
+        df.to_excel(towrite, encoding="utf-8", index=False, header=True)  # write to BytesIO buffer
+        towrite.seek(0)  # reset pointer
+        b64 = base64.b64encode(towrite.read()).decode()
+        href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="data_download.xlsx">Download Excel File</a>'
+        return st.markdown(href, unsafe_allow_html=True)
+
+    
+    
     st.subheader('Downloads:')
     generate_excel_download_link(df_grouped_FBL3N)
     generate_html_download_link(fig)
