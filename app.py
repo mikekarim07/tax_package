d724ead265fdfb45a9f2f25a051bba41e8346b49
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2023-06-12 10:43:59 -0600

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ if Parametros_uploaded_file:
     def generate_excel_download_link(df_grouped_FBL3N):
         # Credit Excel: https://discuss.streamlit.io/t/how-to-add-a-download-excel-csv-function-to-a-button/4474/5
         towrite = BytesIO()
-        df_grouped_FBL3N.to_excel(towrite, encoding="utf-8", index=False, header=True)  # write to BytesIO buffer
+        df_grouped_FBL3N.to_excel(towrite, index=False, header=True)  # write to BytesIO buffer
         towrite.seek(0)  # reset pointer
         b64 = base64.b64encode(towrite.read()).decode()
         href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="data_download.xlsx">Download Excel File</a>'
