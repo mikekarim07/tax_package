cf5f1035df20360719c6cfd969d882b2d92ca3e6
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2023-06-13 16:21:59 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ import base64
 from io import StringIO, BytesIO
 from streamlit_option_menu import option_menu
 
-
+#with st.sidebar:
+#    selected = option_menu(
+#        menu_title = "Company Code",
+#        options = [df2],
+#    )
 
 #tab_titles = [
 #    'primera',
@@ -53,11 +57,7 @@ if Parametros_uploaded_file:
     df2 = pd.unique(df_FBL3N[['Company Code']].values.ravel())
     #st.dataframe(df2)
     
-    with st.sidebar:
-    selected = option_menu(
-        menu_title = "Company Code",
-        options = [df2],
-    )
+    
     
     #cocode = st.selectbox('Company Code',df2)
     cocode = df_FBL3N['Company Code'] == st.selectbox('Company Code',df2)
