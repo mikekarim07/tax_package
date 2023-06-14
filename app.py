faf04503580b9f3f3fcefab121cc4311b9be3252
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2023-06-14 10:09:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -43,59 +43,80 @@ if Parametros_uploaded_file:
     st.dataframe(df_parametros)
     st.write(df_parametros.shape)
 
-    groupby_column = st.selectbox(
-        'What would you like to analyse?',
-        ('Company Code', 'Account', 'User Name', 'Tax Code'),
+
+    edited_df = st.data_editor(
+    df_FBL3N,
+    column_config={
+        "Company Code": "CoCode",
+        "Document Number": st.column_config.SelectboxColumn(
+            "Doc Number",
+            help="Clasifica el Num de documento",
+            width="medium",
+            options=[
+                "Venta",
+                "Compra",
+                "Hedge",
+            ],,
+        ),
+        "is_widget": "Widget ?",
+    },
+    disabled=["command", "is_widget"],
+    hide_index=True,
     )
+    
+    #groupby_column = st.selectbox(
+    #    'What would you like to analyse?',
+    #    ('Company Code', 'Account', 'User Name', 'Tax Code'),
+    #)
 
     
     
     
     # -- GROUP DATAFRAME
-    output_columns = ['Amount in local currency']
-    df_grouped_FBL3N = df_FBL3N.groupby(by=[groupby_column], as_index=False)[output_columns].sum()
-    #st.dataframe(df_grouped_FBL3N)
+    #output_columns = ['Amount in local currency']
+    #df_grouped_FBL3N = df_FBL3N.groupby(by=[groupby_column], as_index=False)[output_columns].sum()
+    ##st.dataframe(df_grouped_FBL3N)
 
     # -- Información filtrada por company code y agrupada
-    df2 = pd.unique(df_FBL3N[['Company Code']].values.ravel())
-    #st.dataframe(df2)
+    #df2 = pd.unique(df_FBL3N[['Company Code']].values.ravel())
+    ##st.dataframe(df2)
     
     
     
-    #cocode = st.selectbox('Company Code',df2)
-    cocode = df_FBL3N['Company Code'] == st.selectbox('Choose all Company Codes', df2)
+    ##cocode = st.selectbox('Company Code',df2)
+    #cocode = df_FBL3N['Company Code'] == st.selectbox('Choose all Company Codes', df2)
         
-    st.subheader('Auxiliar FBL3N Filtrado por Company code')
-    df_FBL_filtered = df_FBL3N[[cocode]]
-    st.dataframe(df_FBL_filtered)
+    #st.subheader('Auxiliar FBL3N Filtrado por Company code')
+    #df_FBL_filtered = df_FBL3N[[cocode]]
+    #st.dataframe(df_FBL_filtered)
     
     
-    st.subheader('Gráfica')
-    # -- PLOT DATAFRAME
-    fig = px.bar(
-        df_grouped_FBL3N,
-        x=groupby_column,
-        y='Amount in local currency',
-        color='Amount in local currency',
-        color_continuous_scale=['purple', 'green'],
-        template='plotly_white',
-        title=f'<b>Sales & Profit by {groupby_column}</b>'
-    )
-    st.plotly_chart(fig)
+    #st.subheader('Gráfica')
+    ## -- PLOT DATAFRAME
+    #fig = px.bar(
+    #    df_grouped_FBL3N,
+    #    x=groupby_column,
+    #    y='Amount in local currency',
+    #    color='Amount in local currency',
+    #    color_continuous_scale=['purple', 'green'],
+    #    template='plotly_white',
+    #    title=f'<b>Sales & Profit by {groupby_column}</b>'
+    #)
+    #st.plotly_chart(fig)
 
-    # -- DOWNLOAD SECTION
+    ## -- DOWNLOAD SECTION
     
-    def generate_excel_download_link(df_grouped_FBL3N):
+    #def generate_excel_download_link(df_grouped_FBL3N):
         # Credit Excel: https://discuss.streamlit.io/t/how-to-add-a-download-excel-csv-function-to-a-button/4474/5
-        towrite = BytesIO()
-        df_grouped_FBL3N.to_excel(towrite, index=False, header=True)  # write to BytesIO buffer
-        towrite.seek(0)  # reset pointer
-        b64 = base64.b64encode(towrite.read()).decode()
-        href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="data_download.xlsx">Download Excel File</a>'
-        return st.markdown(href, unsafe_allow_html=True)
+    #    towrite = BytesIO()
+    #    df_grouped_FBL3N.to_excel(towrite, index=False, header=True)  # write to BytesIO buffer
+    #    towrite.seek(0)  # reset pointer
+    #    b64 = base64.b64encode(towrite.read()).decode()
+    #    href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="data_download.xlsx">Download Excel File</a>'
+    #    return st.markdown(href, unsafe_allow_html=True)
 
     
     
-    st.subheader('Downloads:')
-    generate_excel_download_link(df_grouped_FBL3N)
-    #generate_html_download_link(fig)
+    #st.subheader('Downloads:')
+    #generate_excel_download_link(df_grouped_FBL3N)
+    ##generate_html_download_link(fig)
