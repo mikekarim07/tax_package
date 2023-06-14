386ef2f66c590400bfd02b81e8a24eaae9a34d6a
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2023-06-14 10:32:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ if Parametros_uploaded_file:
     st.write(df_parametros.shape)
     
     Ctas_unicas = pd.unique(df_FBL3N[['Account']].values.ravel())
-    result = pd.merge(Ctas_unicas, Parametros, left_on = 'Account', right_on = 'GL_Account', how = 'left')
+    result = pd.merge(Ctas_unicas, df_parametros, left_on = 'Account', right_on = 'GL_Account', how = 'left')
     st.dataframe('result')
 
 
