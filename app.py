8d4648341a7288736f6eece0fc9ab2a53e391382
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2023-06-23 12:32:06 -0600

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ if Parametros_uploaded_file:
             "Doc Number",
             help="Clasifica el Num de documento",
             width="medium",
-            options=[[Company Codes]],
+            options=[Company_codes],
         ),
         "is_widget": "Widget ?",
     },
