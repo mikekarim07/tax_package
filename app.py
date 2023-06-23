c6011ec3fb4b06023ba072d3cf88f2b6ca230a92
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2023-06-23 14:10:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ if Parametros_uploaded_file:
     FBL3N_merged = FBL3N_merged.rename(columns={"CoCd": "Related Party"})
     st.dataframe(FBL3N_merged)
 
-    FBL3N_merged = FBL3N_merged.groupby("Company Code", "CoCd").sum()
+    FBL3N_merged = FBL3N_merged.groupby("Company Code").sum()
     st.table(FBL3N_merged)
 
     #codigo para editar el dataframe result considerando que hay que agregar la descripcion, en el Country y Cocode que sea un selectbox
