8741a88a77b6e3d3205da32dea40d839ecd5d07b
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2023-12-31 14:00:44 -0600

    Update app.py

--- app.py
+++ app.py
@@ -244,12 +244,12 @@ if uploaded_FBL3N_train and uploaded_new_FBL3N and uploaded_masters:
     # columns_to_rename = {'Col1': 'NewCol1', 'Col2': 'NewCol2'}
     # df = df.rename(columns=columns_to_rename)
 
-    # columns_to_eliminate = ['Status', 'V', 'ML', 'Subcode_td_1', 'Subcode_ML', 'GL_Account', 'Description', 'Country', 'Code_Type', 'Code', 'Code_Desc',
-    #                         'Code_Type_RP', 'Code_RP', 'Code_Desc_RP', 'SC_1', 'SC_2', 'SC_3', 'SC_4', 'SC_5', 'SC_6', 'SC_7', 'SC_8', 'SC_concat',
-    #                        'SC_Fix', 'Subcode_td', 'Subcode_assigned', 'conteo']
-    # FBL3N_new = FBL3N_new.drop(columns=columns_to_eliminate)
-    # columns_to_rename = {'CoCd': 'Related Party'}
-    # FBL3N_new = FBL3N_new.rename(columns=columns_to_rename)
+    columns_to_eliminate = ['Status', 'V', 'ML', 'Subcode_td_1', 'Subcode_ML', 'GL_Account', 'Description', 'Country', 'Code_Type', 'Code', 'Code_Desc',
+                            'Code_Type_RP', 'Code_RP', 'Code_Desc_RP', 'SC_1', 'SC_2', 'SC_3', 'SC_4', 'SC_5', 'SC_6', 'SC_7', 'SC_8', 'SC_concat',
+                           'SC_Fix', 'Subcode_td', 'Subcode_assigned', 'conteo']
+    FBL3N_new = FBL3N_new.drop(columns=columns_to_eliminate)
+    columns_to_rename = {'CoCd': 'Related Party'}
+    FBL3N_new = FBL3N_new.rename(columns=columns_to_rename)
 
     
     FBL3N_new = FBL3N_new[['CONCAT', 'Subcode', 'Related Party', 'Company Code', 'Document Number', 'Document Type', 'Account', 'Text', 'Reference', 'Document Header Text',
