bf2e689eee3c50a4e0d1390cd1116a7496ddc64a
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-01-24 13:47:08 -0600

    Update validate_fbl3n.py

--- validate_fbl3n.py
+++ validate_fbl3n.py
@@ -65,9 +65,10 @@ if file is not None:
     # Filtros
     company_code_filter = st.sidebar.multiselect("Seleccionar Company Code:", unique_company_codes)
     related_party_filter = st.sidebar.multiselect("Seleccionar Related Party:", unique_related_parties)
-
+    st.write(company_code_filter)
     # Aplicar filtros
     filtered_FBL3N_classified = apply_filters(FBL3N_classified, company_code_filter, related_party_filter)
+    # filtered_FBL3N_classified = FBL3N_classified[FBL3N_classified['Company Code'].isin(company_code_filter)]
     merged_FBL3N_classified = FBL3N_classified.merge(FBL3N_classified, left_on="Key_1", right_on='Key_2', how='outer', suffixes=(' sell', ' purchase'))
     st.write(merged_FBL3N_classified.columns)
     merged_FBL3N_classified = merged_FBL3N_classified[merged_FBL3N_classified['Company Code sell'].isin(company_code_filter)]
