4eff458d4c43092ba26834f02b5086e0adac220e
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-01-24 13:03:38 -0600

    Update validate_fbl3n.py

--- validate_fbl3n.py
+++ validate_fbl3n.py
@@ -14,6 +14,7 @@ import base64
 import xlsxwriter
 from xlsxwriter import Workbook
 import time
+import pygwalker as pyg
 
 
 
@@ -68,9 +69,10 @@ if file is not None:
     filtered_FBL3N_classified = apply_filters(FBL3N_classified, company_code_filter, related_party_filter)
     merged_FBL3N_classified = FBL3N_classified.merge(FBL3N_classified, left_on="Key_1", right_on='Key_2', how='outer', suffixes=(' sell', ' purchase'))
     st.write(merged_FBL3N_classified.columns)
-    merged_FBL3N_classified = merged_FBL3N_classified[(merged_FBL3N_classified['Company Code sell'].isin(company_code_filter)) & (merged_FBL3N_classified['Related Party purchase'].isin(company_code_filter))]
+    # merged_FBL3N_classified = merged_FBL3N_classified[(merged_FBL3N_classified['Company Code sell'].isin(company_code_filter)) & (merged_FBL3N_classified['Related Party purchase'].isin(company_code_filter))]
 
     # Mostrar el DataFrame filtrado
     st.dataframe(filtered_FBL3N_classified)
-    st.dataframe(merged_FBL3N_classified)
+    # st.dataframe(merged_FBL3N_classified)
+    pyg.walk(filtered_FBL3N_classified)
 
