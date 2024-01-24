20b555f4f026d4b257aaed8bf791da752ab49df5
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-01-23 19:22:29 -0600

    Update validate_fbl3n.py

--- validate_fbl3n.py
+++ validate_fbl3n.py
@@ -55,14 +55,12 @@ if st.session_state.FBL3N_classified is None and uploaded_FBL3N_classified is no
     st.session_state.FBL3N_classified = load_FBL3N(uploaded_FBL3N_classified)
 
 if st.session_state.FBL3N_classified is not None:
-    # col1, col2 = st.columns.sidebar(2)
 
-    # with col1:
     selected_company_code = st.selectbox("Selecciona el Company Code", st.session_state.FBL3N_classified['Company Code'].unique())
-    filtered_df = st.session_state.FBL3N_classified[st.session_state.FBL3N_classified['Company Code'] == selected_company_code]
-    # with col2:
+    st.session_state.FBL3N_classified = st.session_state.FBL3N_classified[st.session_state.FBL3N_classified['Company Code'] == selected_company_code]
+
     selected_related_party = st.selectbox("Selecciona el Related Party", options=st.session_state.FBL3N_classified['Related Party'].unique(),)
-    filtered_df = st.session_state.FBL3N_classified[st.session_state.FBL3N_classified['Related Party'] == selected_related_party]
+    st.session_state.FBL3N_classified = st.session_state.FBL3N_classified[st.session_state.FBL3N_classified['Related Party'] == selected_related_party]
 
     st.dataframe(filtered_df)
 
