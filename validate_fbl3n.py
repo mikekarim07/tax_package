aacf1d4435645de455b830e1f9d874c3a6434078
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-01-24 12:47:40 -0600

    Update validate_fbl3n.py

--- validate_fbl3n.py
+++ validate_fbl3n.py
@@ -67,6 +67,7 @@ if file is not None:
     # Aplicar filtros
     filtered_FBL3N_classified = apply_filters(FBL3N_classified, company_code_filter, related_party_filter)
     merged_FBL3N_classified = FBL3N_classified.merge(FBL3N_classified, left_on="Key_1", right_on='Key_2', how='outer', suffixes=(' sell', ' purchase'))
+    st.write(merged_FBL3N_classified.columns)
     merged_FBL3N_classified = merged_FBL3N_classified[(merged_FBL3N_classified['Company Code sell'].isin(company_codes)) & (merged_FBL3N_classified['Company Code purchase'].isin(related_parties))]
     # Mostrar el DataFrame filtrado
     st.dataframe(filtered_FBL3N_classified)
