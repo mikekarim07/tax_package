56b521257305788719275cc59ba5d8abd1de230b
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-01-25 09:59:49 -0600

    Update validate_fbl3n.py

--- validate_fbl3n.py
+++ validate_fbl3n.py
@@ -39,36 +39,31 @@ st.subheader('Related Party Operations validations')
 # Función para cargar el DataFrame desde el archivo Excel
 @st.cache_data
 def load_data(file):
-    FBL3N_classified = pd.read_excel(file, sheet_name='FBL3N')
+    FBL3N_classified = pd.read_excel(file, sheet_name='FBL3N', dtype = {'Subcode': str, 'Company Code': str, 'Document Type': str, 'Document Number': str, 'Account': str, 'Text': str,
+                                        'Reference': str, 'Document Header Text': str, 'User Name': str, 'Tax Code': str,})
     return FBL3N_classified
 
-# Función para aplicar filtros
-@st.cache_data
-def apply_filters(FBL3N_classified, company_codes, related_parties):
-    if company_codes:
-        FBL3N_classified = FBL3N_classified[FBL3N_classified['Company Code'].isin(company_codes)]
-    if related_parties:
-        FBL3N_classified = FBL3N_classified[FBL3N_classified['Related Party'].isin(related_parties)]
-    return FBL3N_classified
 
 # Cargar el archivo Excel
-file = st.file_uploader("Subir archivo Excel", type=["xlsx"])
+file = st.file_uploader("Upload the FBL3N file categorized for validation", type=["xlsx"])
 
 if file is not None:
     # Cargar el DataFrame desde el archivo Excel
     FBL3N_classified = load_data(file)
 
-    # Obtener los valores únicos de las columnas "Company Code" y "Related Party"
-    unique_company_codes = FBL3N_classified['Company Code'].unique()
-    unique_related_parties = FBL3N_classified['Related Party'].unique()
+    # # Obtener los valores únicos de las columnas "Company Code" y "Related Party"
+    # unique_company_codes = FBL3N_classified['Company Code'].unique()
+    # unique_related_parties = FBL3N_classified['Related Party'].unique()
 
     # Filtros
-    company_code_filter = st.sidebar.multiselect("Seleccionar Company Code:", unique_company_codes)
-    related_party_filter = st.sidebar.multiselect("Seleccionar Related Party:", unique_related_parties)
+    company_code_filter = st.sidebar.multiselect("Seleccionar Company Code:", FBL3N_classified['Company Code'].unique())
+    related_party_filter = st.sidebar.multiselect("Seleccionar Related Party:", FBL3N_classified['Related Party'].unique())
     st.write(company_code_filter)
+    
     # Aplicar filtros
     # filtered_FBL3N_classified = apply_filters(FBL3N_classified, company_code_filter, related_party_filter)
     # filtered_FBL3N_classified = FBL3N_classified[FBL3N_classified['Company Code'].isin(company_code_filter)]
+    
     filtered_FBL3N_classified = FBL3N_classified[(FBL3N_classified['Company Code'].isin(company_code_filter)) & (FBL3N_classified['Related Party'].isin(related_party_filter))]
     # merged_FBL3N_classified = FBL3N_classified.merge(FBL3N_classified, left_on="Key_1", right_on='Key_2', how='outer', suffixes=(' sell', ' purchase'))
     merged_FBL3N_classified = FBL3N_classified.merge(FBL3N_classified, left_on="Key_1", right_on='Key_2', how='outer', suffixes=('', ' purchase'))
