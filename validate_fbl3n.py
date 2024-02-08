6653ed4262e1e8508091af9d3d40c8b1b9ae9188
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-02-08 08:29:17 -0600

    Update validate_fbl3n.py

--- validate_fbl3n.py
+++ validate_fbl3n.py
@@ -36,6 +36,18 @@ st.image("https://www.kellanovaus.com/content/dam/NorthAmerica/kellanova-us/imag
 st.header('Tax Package Model')
 st.subheader('Related Party Operations validations')
 
+
+def highlight_rows(val):
+    if FBL3N_merged_filtered['Subcode'] == 0:
+        return 'background-color: yellow'
+    elif FBL3N_merged_filtered['Subcode'] == 300:
+        return 'background-color: lightgreen'
+    else:
+        return ''
+
+
+
+
 # Función para cargar el DataFrame desde el archivo Excel
 @st.cache_data
 def load_data(file):
@@ -63,79 +75,22 @@ if upload_FBL3N is not None:
         FBL3N_merged_filtered = FBL3N_classified.merge(FBL3N_classified, left_on="Key_1", right_on='Key_2', how='outer', suffixes=('', ' expense'))
         # FBL3N_merged_filtered = FBL3N_merged_filtered[((FBL3N_merged_filtered['Company Code'].isin(company_code_filter)) | (FBL3N_merged_filtered['Company Code'].isna())) & ((FBL3N_merged_filtered['Related Party expense'].isin(company_code_filter)) | (FBL3N_merged_filtered['Related Party expense'].isna()))]
         FBL3N_merged_filtered = FBL3N_merged_filtered[((FBL3N_merged_filtered['Company Code'] == company_code_filter) | (FBL3N_merged_filtered['Company Code'].isna())) & ((FBL3N_merged_filtered['Related Party expense'] == company_code_filter) | (FBL3N_merged_filtered['Related Party expense'].isna()))]
+        # FBL3N_merged_filtered = [[
+        # Apply conditional formatting using the style.apply() function
+        styled_df = FBL3N_merged_filtered.style.apply(lambda x: x.apply(highlight_rows, axis=1), axis=None)
+        
+        # Display the styled DataFrame in Streamlit
+        st.write('FBL3N merged & filtered with conditinal formatting')
+        st.dataframe(styled_df, unsafe_allow_html=True)
+
+
+        edited_df = st.data_editor(FBL3N_merged_filtered, disabled=["Related Party sell", "Company Code sell"], hide_index=True)
+        FBL3N_classified.update(merged_FBL3N_classified)
+
+        
         # Mostrar el DataFrame filtrado
         st.write('FBL3N merged & filtered')
         st.dataframe(FBL3N_merged_filtered)
-
-
-
-
-# import streamlit as st
-# import pandas as pd
-# from sklearn.model_selection import train_test_split
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# from sklearn.naive_bayes import MultinomialNB
-# from sklearn.metrics import accuracy_score
-# import pickle
-# import os
-# import datetime
-# from io import BytesIO
-# import io
-# from io import StringIO
-# import base64
-# import xlsxwriter
-# from xlsxwriter import Workbook
-# import time
-# # import pygwalker as pyg
-# # import streamlit.components.v1 as components
-# # from pygwalker.api.streamlit import init_streamlit_comm, get_streamlit_html
-
-
-
-# st.set_page_config(
-#     page_title="Tax Package Model",
-#     page_icon="📈",
-#     layout="wide",
-#     initial_sidebar_state="expanded",
-#     menu_items={
-#         'Get Help': 'mailto:miguel.karim@karimortega.com'
-#     }
-# )
-
-
-
-# st.image("https://www.kellanovaus.com/content/dam/NorthAmerica/kellanova-us/images/logo.svg", width=120)
-# # st.header('Tax Package Model')
-# st.subheader('Related Party Operations validations')
-
-# # Función para cargar el DataFrame desde el archivo Excel
-# @st.cache_data
-# def load_data(file):
-#     FBL3N_classified = pd.read_excel(file, sheet_name='FBL3N', dtype = {'Subcode': str, 'Company Code': str, 'Document Type': str, 'Document Number': str, 'Account': str, 'Text': str,
-#                                         'Reference': str, 'Document Header Text': str, 'User Name': str, 'Tax Code': str,})
-#     return FBL3N_classified
-
-
-# upload_FBL3N = st.sidebar.file_uploader("Upload the FBL3N file categorized for validation", type=["xlsx"])
-
-# if upload_FBL3N is not None:
-#     # Cargar el DataFrame desde el archivo Excel
-#     FBL3N_classified = load_data(upload_FBL3N)
-
-#     #----- Create Company Code Filters
-#     company_code_filter = st.sidebar.multiselect("Seleccionar Company Code:", FBL3N_classified['Company Code'].unique())
-#     # related_party_filter = st.sidebar.multiselect("Seleccionar Related Party:", FBL3N_classified['Related Party'].unique())
-    
-
-#     if not company_code_filter:
-#             # Mostrar todo el DataFrame sin filtros
-#             st.dataframe(FBL3N_classified)
-#     else:
-#         # Aplicar filtros si hay selecciones
-#         # filtered_FBL3N_classified = FBL3N_classified[(FBL3N_classified['Company Code'].isin(company_code_filter)) & (FBL3N_classified['Related Party'].isin(related_party_filter))]
-#         filtered_FBL3N_classified = FBL3N_classified[(FBL3N_classified['Company Code'].isin(company_code_filter))]
-#         # Mostrar el DataFrame filtrado
-#         st.dataframe(filtered_FBL3N_classified)
 
 
 
