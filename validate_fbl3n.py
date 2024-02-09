34d6e9eda388812ed7c369c92e3bcdffea97b93e
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-02-09 08:20:50 -0600

    Update validate_fbl3n.py

--- validate_fbl3n.py
+++ validate_fbl3n.py
@@ -70,11 +70,9 @@ if upload_FBL3N is not None:
     doc_num_filter = st.text_input("Introduce the Document number you want to see the information")
     CC_info = FBL3N_merged_filtered[FBL3N_merged_filtered['Document Number'] == doc_num_filter]
     CC_info = CC_info[['CONCAT', 'Subcode', 'Related Party', 'Company Code', 'Document Number', 'Document Type', 'Account', 'Text', 'Reference', 'Document Header Text', 'User Name', 'Posting period']].T
-    st.caption(CC_info)
-    st.json(CC_info)
+    RP_info = CC_info[['CONCAT expense', 'Subcode expense', 'Related Party expense', 'Company Code expense', 'Document Number expense', 'Document Type expense', 'Account expense', 'Text expense', 'Reference expense', 'Document Header Text expense', 'User Name expense', 'Posting period expense']].T    
     st.write(CC_info)
-    st.magic(CC_info)
-    
+    st.write(RP_info)
     st.write('Dataframe actualizado')
     st.dataframe(FBL3N_merged)
 
