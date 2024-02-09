e16da8ea9615356037b406d86f9e6a32f9807179
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-02-08 18:50:37 -0600

    Update validate_fbl3n.py

--- validate_fbl3n.py
+++ validate_fbl3n.py
@@ -59,8 +59,9 @@ upload_FBL3N = st.sidebar.file_uploader("Upload the FBL3N file categorized for v
 if upload_FBL3N is not None:
     # Cargar el DataFrame desde el archivo Excel
     FBL3N_classified = load_data(upload_FBL3N)
-    company_code_filter = st.sidebar.selectbox("Select Company Code:", FBL3N_classified['Company Code'].unique())
+    
     FBL3N_merged = FBL3N_classified.merge(FBL3N_classified, left_on="Key_1", right_on='Key_2', how='outer', suffixes=('', ' expense'))
+    company_code_filter = st.sidebar.selectbox("Select Company Code:", FBL3N_classified['Company Code'].unique())
     FBL3N_merged_filtered = FBL3N_merged[((FBL3N_merged['Company Code'] == company_code_filter) | (FBL3N_merged['Company Code'].isna())) & ((FBL3N_merged['Related Party expense'] == company_code_filter) | (FBL3N_merged['Related Party expense'].isna()))]
     edited_df = st.data_editor(FBL3N_merged, disabled=["Related Party sell", "Company Code sell"], hide_index=False)
     FBL3N_merged.update(edited_df)
