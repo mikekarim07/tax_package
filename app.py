cffd84c95d3cda36fa70762ae0c46d6ce5c01dd4
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-04-22 13:55:53 -0600

    Update app.py

--- app.py
+++ app.py
@@ -193,7 +193,7 @@ if uploaded_FBL3N_train and uploaded_new_FBL3N and uploaded_masters and uploaded
     
     #---------------FB03-------------
     fb03_NA_Fill_Columns = ['Reversal']
-    fb03[fb03_NA_Fill_Columns] = fb03[fb03_NA_Fill_Columns].fillna('')
+    fb03[fb03_NA_Fill_Columns] = fb03[fb03_NA_Fill_Columns].dropna('')
 
 
 
