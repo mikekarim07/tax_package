9cf51f28617873ee9b6b2c491657db3aff4a378b
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-05-30 08:04:40 -0600

    Update sdosfin.py

--- sdosfin.py
+++ sdosfin.py
@@ -140,7 +140,7 @@ with tab1:
             GIMX_Balances = load_sheet(uploaded_GIMX, sheet_AccBal_GIMX)
             GIMX_Balances = GIMX_Balances.iloc[3:]
             GIMX_Balances.columns = GIMX_Balances.iloc[0]
-            GIMX_Balances = GIMX_Balances[1:].reset_index(drop=True)
+            # GIMX_Balances = GIMX_Balances[1:].reset_index(drop=True)
 
             # GIMX_Balances = GIMX_Balances[GIMX_Balances['Clasificacion'].isin(GIMX_Clasificacion)]
 
