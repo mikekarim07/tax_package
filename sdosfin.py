43856172fe8bd45b393958d76c40029c762d12c1
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-05-30 07:48:10 -0600

    Update sdosfin.py

--- sdosfin.py
+++ sdosfin.py
@@ -134,7 +134,7 @@ with tab1:
 
     with subtab2:
         if uploaded_GIMX and sheet_PnL_GIMX is not "Select":
-            sheet_AccBal_GIMX = st.sidebar.selectbox("Select the sheet which contains GIMX Account Balances", sheet_names_GIMX)
+            sheet_AccBal_GIMX = st.selectbox("Select the sheet which contains GIMX Account Balances", sheet_names_GIMX)
             # GIMX_Balances = load_sheet(uploaded_GIMX, sheet_AccBal_GIMX)
             # st.dataframe(GIMX_Balances)            
             
