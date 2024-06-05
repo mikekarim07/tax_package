97cc29be6ca17d7ad90cd832168fc68224798551
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-06-05 12:11:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -232,12 +232,12 @@ if uploaded_FBL3N_train and uploaded_new_FBL3N and uploaded_masters and uploaded
     hedge_accnts = ZLAAUDIT[ZLAAUDIT['Account'].isin(['7736000018', '7736000019'])]
     hedge_accnts = hedge_accnts[['CONCAT', 'Account']]
     hedge_accnts.rename(columns={'CONCAT': 'Key_ZLAAUDIT', 'Account': 'ZLAAUDIT_Acc'}, inplace=True)
-    st.write('Hedge Accounts')
-    st.dataframe(hedge_accnts)
+    # st.write('Hedge Accounts')
+    # st.dataframe(hedge_accnts)
 
     FBL3N_new = FBL3N_new.merge(hedge_accnts, left_on="CONCAT_01", right_on='Key_ZLAAUDIT', how='left')
-    st.dataframe(FBL3N_new)
-    st.write(FBL3N_new.columns)
+    # st.dataframe(FBL3N_new)
+    # st.write(FBL3N_new.columns)
     
     #---------------Funciones para subcodes fijas-------------------
     
