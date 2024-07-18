2336aa6529deb195a6e585ed566f9ddd54dda92c
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-07-17 18:33:17 -0600

    Update app.py

--- app.py
+++ app.py
@@ -419,7 +419,7 @@ if uploaded_FBL3N_train and uploaded_new_FBL3N and uploaded_masters and uploaded
     def Subcode(row):
         if  pd.isna(row['Subcode']):
             return "ok"
-        else
+        else:
             return "no"
         # # Verificar las condiciones
         # # if row['Subcode'] != '':
