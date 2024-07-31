549391b4b0498334387379074769e440b925f3d3
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-07-31 12:44:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -222,6 +222,8 @@ if uploaded_FBL3N_train and uploaded_new_FBL3N and uploaded_masters and uploaded
         # fb03['Doc. Date'] = pd.to_datetime(fb03['Doc. Date'])
         fb03['doc date year'] = fb03['Doc. Date'].str[:4]
         fb03['clave01'] = fb03['CoCd'] + fb03['DocumentNo'] + fb03['Doc. Date'].str[:4]
+        fb03['clave02'] = fb03['CoCd'] + fb03['Reversal'] + fb03['Doc. Date'].str[:4]
+        fb03_fix = pd.merge(fb03, fb03, left_on='clave01', right_on='clave02', how='left', suffixes=('', '_fix'))
         st.dataframe(fb03)
     fb03_NA_Fill_Columns = ['Reversal']
     fb03 = fb03.dropna(subset=fb03_NA_Fill_Columns)
